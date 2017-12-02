3c3f85b98cde9293c757e28714c5a5429cb84dce
hippunk <arthur.ramolet@hotmail.fr>
2017-12-02 19:27:26 +0100

    player controller + cave generator

---
+++ Assets/Scripts/CharaControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharaControl : MonoBehaviour {
+
+	// Use this for initialization
+	public float speed;
+	private Vector3 target = new Vector3();
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontal = Input.GetAxis ("Horizontal");
+		float vertical = Input.GetAxis ("Vertical");
+		Debug.Log ("hor : "+horizontal+" ver : "+vertical);
+
+
+		target.x = horizontal;
+		target.y = vertical;
+		target.Normalize ();
+
+		transform.position += target* Time.deltaTime*speed;
+	}
+
+	int ClampFloat(float f){
+		return f < 0 ? -1 : f > 0 ? 1 : 0;
+	}
+
+}
---
+++ Assets/Scripts/DragDropProto.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragDropProto : MonoBehaviour {
+
+	public bool overing = false;
+	public bool dragging = false;
+
+	private float distance;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0) && overing) {
+			
+			Debug.Log ("Drag");
+			dragging = true;					
+		}	
+		if (dragging) {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			Vector3 rayPoint = ray.GetPoint(distance);
+			transform.position = rayPoint;
+		}
+		if (Input.GetMouseButtonUp(0) && overing) {
+			dragging = false;
+		}	
+	}
+
+	void OnMouseOver()
+	{
+		if (!overing) {
+			//If your mouse hovers over the GameObject with the script attached, output this message
+			overing = true;
+			Debug.Log ("Mouse is over GameObject.");
+			distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+		}
+	}
+
+	void OnMouseExit()
+	{
+		if(overing && !dragging){
+		//The mouse is no longer hovering over the GameObject so output this message each frame
+			overing = false;
+			Debug.Log("Mouse is no longer on GameObject.");
+		}
+	}
+
+
+}
