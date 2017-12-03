0a11a4dae0d998d8984ec1579c465c4118ce3d45
hippunk <arthur.ramolet@hotmail.fr>
2017-12-03 01:25:20 +0100

    ajout début IA evitement obstacles

--- Assets/Scripts/Enemy/MoveToPlayer.cs
+++ Assets/Scripts/Enemy/MoveToPlayer.cs
@@ -3,24 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveToPlayer : MonoBehaviour {
-	private GameObject target;
+	private GameObject target = null;
 	public float translationSpeed = 2.0f;
 	public float angularSpeed = 2.0f;
+	public float correctionOffset = 2.0f;
 
+	public GameObject objDebug;
+
+	private bool evade = false;
+	private float correctionAngle = 0;
 	// Use this for initialization
 	void Start () {
-
-		target = GameObject.FindGameObjectWithTag ("Player");
-
+		objDebug = new GameObject ();
+		if (target == null)
+			target = GameObject.FindGameObjectWithTag ("Player");
+		else {
+			Debug.Log ("pb chargement Player");
+		}
 			Vector3 lookPos = target.transform.position - transform.position;
+
 			transform.rotation = Quaternion.LookRotation (Vector3.forward, lookPos);
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (target)
 			Move ();
+
 	}
 
 	void Move(){
@@ -31,6 +40,19 @@ public class MoveToPlayer : MonoBehaviour {
 
 
 		Vector3 lookPos = target.transform.position - transform.position;
+		if (evade) {
+			Debug.Log ("evade");
+			if (correctionAngle == 0) {
+				correctionAngle += correctionOffset;
+			} else {
+				correctionAngle += correctionOffset;				
+			}
+
+			Transform tmpTarget = target.transform;
+			tmpTarget.RotateAround (transform.position, Vector3.forward, correctionAngle);
+			objDebug.transform.position = tmpTarget.position;
+			lookPos = tmpTarget.transform.position - transform.position;
+		}
 
 		Quaternion rotation = Quaternion.LookRotation(Vector3.forward,lookPos); //Auto target selected enemy
 		transform.rotation = Quaternion.Slerp (transform.rotation, rotation, Time.deltaTime*angularSpeed);
@@ -38,4 +60,16 @@ public class MoveToPlayer : MonoBehaviour {
 		transform.rotation.SetLookRotation (target.transform.position);
 		transform.Translate (Vector3.up*Time.deltaTime*translationSpeed);
 	}
+		
+
+	/*void OnCollisionEnter(Collision collision)
+	{
+		evade = true;
+		Debug.Log ("enter collider");
+	}
+	void OnCollisionExit(Collision collision)
+	{
+		Debug.Log ("exit collider");
+		evade = false;
+	}*/
 }
