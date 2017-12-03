1e49cb852df263e0c77fd3070ad364c8acb1785c
Antoine Qu <antoine@munch.home>
2017-12-04 00:23:43 +0100

    linked bulletcount to hud

---
+++ Assets/HUDupdateBulletLeft.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDupdateBulletLeft : MonoBehaviour {
+	public Text field;
+	public Text field2;
+	public BulletFire gun;
+	private int left;
+	// Use this for initialization
+	void Start () {
+		left = gun.maxBullet;
+
+		field.text = gun.maxBullet + "/" + gun.maxBullet;
+		field2.text = gun.maxBullet + "/" + gun.maxBullet;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void decreaseBulletCount(){
+		if (left >= 0)
+			left -= 1;
+		field.text = left + "/" + gun.maxBullet;
+		field2.text = left + "/" + gun.maxBullet;
+	}
+}
--- Assets/Scripts/Gunnery/BulletFire.cs
+++ Assets/Scripts/Gunnery/BulletFire.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BulletFire : MonoBehaviour {
 
-
+	public HUDupdateBulletLeft hud;
 	public float fireRate = 0.2f;
 	public int maxBullet = 20;
 	public int curBullet = 0;
